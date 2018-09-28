8b8917679dfdc113d608ff2c6cfb1daa07ef83c0
Zhilin Fu ( Alan Fu ) <alanfu@live.com>
2018-09-28 12:21:41 -0700

    Added Data generating script

---
+++ DataGenerator.js
@@ -0,0 +1,51 @@
+const faker =  require('faker/locale/en_US');
+const fs = require('fs');
+
+console.time('Task completed in'); // Timer starts
+
+var looped = 1;
+var currentIteration = 1;
+var maxChucks = 20; // Change to set Chunks
+var chunkSize = 500000; // Change to set ChunkSize
+// Retain and Release
+function retainer(currentLoop) {
+  if (currentLoop > maxChucks) {
+    console.timeEnd('Task completed in'); // Timer stops
+  }
+  if (currentLoop <= maxChucks) {
+    let collectedWater = ""; // Water is drained
+    for (var i = 0; i < chunkSize; i++) {
+      let productName = `Clean-O-Bot ${currentIteration}`;
+      let price = faker.commerce.price();
+      let imageURL = faker.image.business();
+      let rating = Math.floor(Math.random() * Math.floor(6)); // Random 0 to 5
+      let reviews = faker.random.number();
+      let isPrime = faker.random.boolean();
+      let data = {
+        id: currentIteration,
+        productName: productName,
+        price: price,
+        imageURL1: "https://loremflickr.com/320/240?random=1",
+        imageURL2: "https://loremflickr.com/320/240?random=2",
+        imageURL3: "https://loremflickr.com/320/240?random=3",
+        imageURL4: "https://loremflickr.com/320/240?random=4",
+        imageURL5: "https://loremflickr.com/320/240?random=5",
+        imageURL6: "https://loremflickr.com/320/240?random=6",
+        rating: rating,
+        reviews: reviews,
+        isPrime: isPrime,
+      }
+      currentIteration++;
+      data = JSON.stringify(data);
+      collectedWater += data + "\n";
+    }
+    fs.appendFile('./DO_NOT_OPEN/sdc_data.json', collectedWater, (err) => {
+      if (err) {console.error(err)}
+      console.log(`Currently at loop #${currentLoop}`);
+      looped++;
+      retainer(looped);
+    });
+  }
+}
+
+retainer(looped);
