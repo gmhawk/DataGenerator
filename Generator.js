0b071713253588927d3e621f4874db3e63ee40cf
Zhilin Fu ( Alan Fu ) <alanfu@live.com>
2018-10-10 20:09:57 -0700

    Updated Generator

--- Generator.js
+++ Generator.js
@@ -5,8 +5,8 @@ console.time('Task completed in'); // Timer starts
 
 var looped = 1;
 var currentIteration = 1;
-var maxChucks = 20; // Change to set Chunks
-var chunkSize = 500000; // Change to set ChunkSize
+var maxChucks = 40; // Change to set Chunks
+var chunkSize = 250000; // Change to set ChunkSize
 // Retain and Release
 function retainer(currentLoop) {
   if (currentLoop > maxChucks) {
@@ -25,11 +25,17 @@ function retainer(currentLoop) {
                   `${productName},` +
                   `${price},` +
                   `https://loremflickr.com/320/240?random=1,` +
+                  `${faker.commerce.product()},` +
                   `https://loremflickr.com/320/240?random=2,` +
+                  `${faker.commerce.product()},` +
                   `https://loremflickr.com/320/240?random=3,` +
+                  `${faker.commerce.product()},` +
                   `https://loremflickr.com/320/240?random=4,` +
+                  `${faker.commerce.product()},` +
                   `https://loremflickr.com/320/240?random=5,` +
+                  `${faker.commerce.product()},` +
                   `https://loremflickr.com/320/240?random=6,` +
+                  `${faker.commerce.product()},` +
                   `${rating},` +
                   `${reviews},` +
                   `${isPrime}`;
